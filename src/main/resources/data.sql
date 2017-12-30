7188d53e207a689dfdec69f82563dce5f776f301
AbhiJoshi32 <abhishek.joshi2015@vit.ac.in>
2017-12-30 23:28:37 +0530

    update hhdl to create

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,3 +19,22 @@ INSERT INTO oauth_client_details
 VALUES
 	('barClientIdPassword', 'secret', 'bar,read,write',
 	'password,authorization_code,refresh_token', null, null, 36000, 36000, null, true);
+
+INSERT INTO users(
+	user_id, name, username, password, address, email, is_verified, phone, is_active)
+VALUES (1, 'abhi', 'john123', 123, 'India', 'aa@aa.aa', TRUE , 7777777777, 1);
+
+INSERT INTO role(
+	role_id, role_name)
+VALUES (1, 'ADMIN');
+
+INSERT INTO role(
+	role_id, role_name)
+VALUES (2, 'USER');
+
+INSERT INTO role(
+	role_id, role_name)
+VALUES (3, 'SHOP');
+
+INSERT INTO user_role(user_id,role_id)
+	VALUES (1,2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -63,3 +63,48 @@ create table if not exists ClientDetails (
   additionalInformation VARCHAR(4096),
   autoApproveScopes VARCHAR(255)
 );
+
+CREATE TABLE if not exists users
+(
+  user_id integer NOT NULL,
+  name character varying(255) NOT NULL,
+  username character varying(255) NOT NULL,
+  password character varying(255) NOT NULL,
+  address character varying(255),
+  email character varying(255),
+  is_verified boolean,
+  phone character varying(12) NOT NULL,
+  is_active integer,
+  CONSTRAINT user_pkey PRIMARY KEY (user_id)
+);
+
+CREATE TABLE if NOT EXISTS user_role
+(
+  user_id integer NOT NULL,
+  role_id integer NOT NULL,
+  CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role_id),
+  CONSTRAINT uk_it77eq964jhfqtu54081ebtio UNIQUE (role_id),
+  CONSTRAINT fka68196081fvovjhkek5m97n3y FOREIGN KEY (role_id)
+  REFERENCES role (role_id) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION,
+  CONSTRAINT fkj345gk1bovqvfame88rcx7yyx FOREIGN KEY (user_id)
+  REFERENCES users (user_id) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION,
+  CONSTRAINT user_role_role_id_fkey FOREIGN KEY (role_id)
+  REFERENCES role (role_id) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION,
+  CONSTRAINT user_role_user_id_fkey FOREIGN KEY (user_id)
+  REFERENCES users (user_id) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS role
+(
+  role_id integer NOT NULL,
+  role_name character varying(50) NOT NULL,
+  CONSTRAINT role_pkey PRIMARY KEY (role_id)
+);
